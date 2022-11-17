29195a29c4eb85a80da78ca60210aecda7cce2eb
Darko Gjorgjijoski <dg@darkog.com>
2022-11-17 09:24:12 +0100

    Remove leftover MessageService

--- resources/js/services/MessageService.ts
+++
@@ -1,9 +0,0 @@
-import ModelService from "@/services/ModelService";
-
-export default class MessageService extends ModelService {
-    constructor() {
-        super();
-        this.url = '/messages';
-    }
-
-}
