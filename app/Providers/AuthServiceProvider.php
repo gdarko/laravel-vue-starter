877012606fedb4b1d6588b34e665310214cfec7b
Darko Gjorgjijoski <dg@darkog.com>
2023-02-19 15:18:30 +0100

    Upgrade to Laravel 10

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -24,8 +24,6 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->registerPolicies();
-
         ResetPassword::createUrlUsing(function ($user, string $token) {
           return env('SPA_URL') . '/reset-password?token=' . $token;
         });
