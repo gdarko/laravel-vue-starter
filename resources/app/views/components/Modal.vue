32afb23a3b3eab4346c205e61d9eb537c8fe71bc
Darko Gjorgjijoski <dg@darkog.com>
2026-03-29 10:00:00 +0200

    Expand Heroicons icon set to 47 icons, fix modal with teleport and title prop

--- resources/app/views/components/Modal.vue
+++ resources/app/views/components/Modal.vue
@@ -1,18 +1,23 @@
 <template>
-    <dialog class="modal" :class="{'modal-open': $props.isShowing}">
-        <div class="modal-box max-w-2xl">
-            <button v-if="$props.showClose" class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2" @click.prevent="onClose">✕</button>
-            <template v-if="$props.isLoading">
-                <div class="py-10 text-center">
-                    <Spinner/>
-                </div>
-            </template>
-            <slot v-else></slot>
-        </div>
-        <form method="dialog" class="modal-backdrop" @click="onClose">
-            <button>close</button>
-        </form>
-    </dialog>
+    <Teleport to="body">
+        <dialog class="modal" :class="{'modal-open': $props.isShowing}">
+            <div class="modal-box max-w-lg relative">
+                <button v-if="$props.showClose" class="btn btn-sm btn-circle btn-ghost absolute right-3 top-3 z-10" @click.prevent="onClose">✕</button>
+                <template v-if="$props.isLoading">
+                    <div class="py-16 text-center">
+                        <Spinner/>
+                    </div>
+                </template>
+                <template v-else>
+                    <h3 v-if="$props.title" class="font-bold text-lg mb-4 pr-8">{{ $props.title }}</h3>
+                    <slot></slot>
+                </template>
+            </div>
+            <form method="dialog" class="modal-backdrop" @click="onClose">
+                <button>close</button>
+            </form>
+        </dialog>
+    </Teleport>
 </template>
 
 <script>
@@ -23,6 +28,10 @@ export default defineComponent({
     components: {Spinner},
     emits: ['update:isShowing', 'close'],
     props: {
+        title: {
+            type: String,
+            default: '',
+        },
         type: {
             type: String,
             default: 'simple',
--- resources/app/views/components/icons/Icon.vue
+++ resources/app/views/components/icons/Icon.vue
@@ -3,6 +3,16 @@
 </template>
 
 <script>
+/**
+ * Icon wrapper for Heroicons (https://heroicons.com)
+ *
+ * Usage:  <Icon name="users" class="h-5 w-5" />
+ *
+ * To add a new icon:
+ *   1. Browse all icons at https://heroicons.com
+ *   2. Import it from '@heroicons/vue/24/outline' (or /solid, /mini)
+ *   3. Add a short name → component entry to the iconMap below
+ */
 import {computed, defineComponent} from "vue";
 import {
     Bars3Icon,
@@ -25,9 +35,40 @@ import {
     Squares2X2Icon,
     SunIcon,
     MoonIcon,
+    CheckIcon,
+    ExclamationTriangleIcon,
+    InformationCircleIcon,
+    MagnifyingGlassIcon,
+    Cog6ToothIcon,
+    BellIcon,
+    ArrowPathIcon,
+    EyeIcon,
+    EyeSlashIcon,
+    DocumentTextIcon,
+    FolderIcon,
+    PhotoIcon,
+    LinkIcon,
+    CalendarIcon,
+    ClockIcon,
+    MapPinIcon,
+    PhoneIcon,
+    GlobeAltIcon,
+    ShieldCheckIcon,
+    KeyIcon,
+    LockClosedIcon,
+    StarIcon,
+    HeartIcon,
+    ChatBubbleLeftIcon,
+    ArrowUpTrayIcon,
+    ArrowDownOnSquareIcon,
+    ClipboardDocumentIcon,
+    TagIcon,
+    BookmarkIcon,
 } from '@heroicons/vue/24/outline';
 
+// Browse all 300+ icons at https://heroicons.com
 const iconMap = {
+    // Navigation
     'bars': Bars3Icon,
     'times': XMarkIcon,
     'close': XMarkIcon,
@@ -38,19 +79,62 @@ const iconMap = {
     'angle-right': ChevronRightIcon,
     'angle-left': ChevronLeftIcon,
     'home': HomeIcon,
-    'cloud-upload': CloudArrowUpIcon,
-    'filter': FunnelIcon,
+
+    // Actions
     'plus': PlusIcon,
     'edit': PencilSquareIcon,
     'trash': TrashIcon,
     'save': ArrowDownTrayIcon,
-    'envelope': EnvelopeIcon,
-    'paperclip': PaperClipIcon,
-    'tachometer': Squares2X2Icon,
-    'users': UsersIcon,
-    'sign-out': ArrowRightStartOnRectangleIcon,
+    'check': CheckIcon,
+    'search': MagnifyingGlassIcon,
+    'refresh': ArrowPathIcon,
+    'upload': ArrowUpTrayIcon,
+    'download': ArrowDownOnSquareIcon,
+    'clipboard': ClipboardDocumentIcon,
+    'cloud-upload': CloudArrowUpIcon,
+
+    // UI
+    'filter': FunnelIcon,
     'sun': SunIcon,
     'moon': MoonIcon,
+    'eye': EyeIcon,
+    'eye-off': EyeSlashIcon,
+    'settings': Cog6ToothIcon,
+    'bell': BellIcon,
+    'star': StarIcon,
+    'heart': HeartIcon,
+    'bookmark': BookmarkIcon,
+    'tag': TagIcon,
+
+    // Content
+    'envelope': EnvelopeIcon,
+    'paperclip': PaperClipIcon,
+    'document': DocumentTextIcon,
+    'folder': FolderIcon,
+    'photo': PhotoIcon,
+    'link': LinkIcon,
+    'chat': ChatBubbleLeftIcon,
+
+    // Status
+    'warning': ExclamationTriangleIcon,
+    'info': InformationCircleIcon,
+    'shield': ShieldCheckIcon,
+
+    // People & Auth
+    'users': UsersIcon,
+    'sign-out': ArrowRightStartOnRectangleIcon,
+    'key': KeyIcon,
+    'lock': LockClosedIcon,
+
+    // Time & Place
+    'calendar': CalendarIcon,
+    'clock': ClockIcon,
+    'map-pin': MapPinIcon,
+    'phone': PhoneIcon,
+    'globe': GlobeAltIcon,
+
+    // Misc
+    'tachometer': Squares2X2Icon,
 };
 
 export default defineComponent({
